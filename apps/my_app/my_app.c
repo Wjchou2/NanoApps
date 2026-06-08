63b5e918f5bd0db202c7a848d1903b25d20925da
WilliamChou123 <wchou28@shschools.org>
2026-06-08 13:48:51 -0700

    ihi

--- apps/my_app/my_app.c
+++ apps/my_app/my_app.c
@@ -8,6 +8,8 @@ static void changed(lv_event_t *e) {
     
     int value = lv_slider_get_value(slider);
     int brightness = value * 255 / 100;
+    brightness = hb_brightness_get();
+
     hb_brightness_set(brightness);
 
     lv_label_set_text_fmt(label,"%d", value);
