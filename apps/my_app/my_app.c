68658876eba5d11f81b145fc3eb601090a459d66
WilliamChou123 <wchou28@shschools.org>
2026-06-08 13:21:07 -0700

    app

---
+++ apps/my_app/my_app.c
@@ -0,0 +1,20 @@
+#include "hb_sdk.h"
+#include "lvgl.h"
+
+static void changed(lv_event_t *e) {
+    lv_obj_t *slider = lv_event_get_target(e);
+    int value = lv_slider_get_value(slider);
+    hb_brightness_set(value);
+}
+
+HB_APP_ENTRY(payload_entry) {
+    lv_obj_t *slider = lv_slider_create(lv_screen_active());
+
+    lv_obj_set_width(slider, 180);
+    lv_obj_align(slider, LV_ALIGN_CENTER, 0, 0);
+
+    lv_slider_set_range(slider, 0, 1);
+    lv_slider_set_value(slider, 0.5, LV_ANIM_OFF);
+
+    lv_obj_add_event_cb(slider, changed, LV_EVENT_VALUE_CHANGED, NULL);
+}
